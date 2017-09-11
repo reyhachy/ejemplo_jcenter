23df0c17c4af19b4839b5e38450facde8588fcfd
JuanReynaldoEscobar <jrescobarmiron@gmail.com>
2017-09-11 18:28:38 -0500

    Upload

---
+++ app/src/main/java/com/totalplay/ejemplorepo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.totalplay.ejemplorepo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.totalplay.ejemplorepo.pojos.Persona;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    Button boton;
+    Persona rey=new Persona();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text= (TextView)findViewById(R.id.textv);
+        boton= (Button)findViewById(R.id.button);
+        rey.nombre="Reynaldo";
+        rey.edad="22";
+        rey.sexo="masculino";
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(new String("nombre = " + rey.nombre + "  edad = " + rey.edad + "   sexo = " + rey.sexo));
+            }
+        });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/totalplay/ejemplorepo/pojos/Persona.java
@@ -0,0 +1,15 @@
+package com.totalplay.ejemplorepo.pojos;
+
+/**
+ * Created by NS-TOTALPLAY on 11/09/2017.
+ */
+
+public class Persona {
+
+    public String nombre;
+
+    public String edad;
+
+    public String sexo;
+
+}
